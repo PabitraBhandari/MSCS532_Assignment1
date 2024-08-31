74d26e619e5ac552af6388ede5f4bcf57fdf2f87
PabitraBhandari <pabitrabhandari19@gmail.com>
2024-08-31 00:05:31 -0500

    main code layout

--- first.py
+++ first.py
@@ -4,3 +4,14 @@
 #Date: 09/01/2024
 #To Do: Sorting array into monotonically decreasing order instead of increasing using Insertion Sort algorithm.
 
+def decreasingOrderInsertionSort(array):
+   
+    for i in range(1, len(array)):
+        out = array[i]
+        
+        j = i - 1
+        while j >= 0 and array[j] < out:
+            array[j + 1] = array[j]
+            j = j- 1
+        array[j + 1] = out
+
